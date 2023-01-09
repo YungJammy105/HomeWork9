f2cf47ae2a31e975084fd32a588412fbf17609cb
Ilya Andreev <Franklylakers@yandex.ru>
2023-01-09 23:25:32 +0700

    add Example68

---
+++ Example68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter number M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number N:");
+int numberN = int.Parse(Console.ReadLine());
+
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine($"Ackermann function for numbers A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
