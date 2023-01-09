41abd0b516778d2d9febb16146dcb17b6b47ea57
Ilya Andreev <Franklylakers@yandex.ru>
2023-01-09 23:22:45 +0700

    add Example66

---
+++ Example66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter number N:");
+int numberM  = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number M:");
+int numberN = int.Parse(Console.ReadLine());
+
+SumNumbersInInterval(numberM, numberN, 0);
+
+void SumNumbersInInterval (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Sum of natural elements between N and M: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    SumNumbersInInterval(numberM, numberN, sum);
+}
