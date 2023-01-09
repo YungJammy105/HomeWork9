9f6cca9409827eb83631ee22ebf3c3e65f0700f0
Ilya Andreev <Franklylakers@yandex.ru>
2023-01-09 23:00:23 +0700

    add Example64

---
+++ Example64/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+OutputAllNaturalNumbersInInterval(num);
+
+void OutputAllNaturalNumbersInInterval(int number)
+{
+    if(number < 0)
+    {
+        Console.WriteLine("Start over and enter a number greater than zero");
+    }
+    else if (number == 0) return;
+    Console.Write("{0,4}", number);
+    OutputAllNaturalNumbersInInterval (number - 1);
+}
